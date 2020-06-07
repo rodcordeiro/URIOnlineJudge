0999b96cd036b91197a3a78b626500fca0d397a1
Rodrigo Cordeiro <rodrigomendoncca@gmail.com>
2020-06-07 00:40:04 -0300

    Update files

--- codes/1018.js
+++ codes/1018.js
@@ -1,5 +1,25 @@
-
+function teste(){
+  let values = [576,11257,503,500,20,73]
+  values.forEach(value => {
+    cedulas(value)
+  })
+}
 
 function cedulas(value){
+  console.log(value);
+  let cedulas = [100,50,20,10,5,2,1]
+  let resto = value
+  cedulas.forEach(cedula =>{
+    let numCelulas = parseInt(resto / cedula)
+    resto = resto % cedula
+    console.log(`${numCelulas} nota(s) de R$ ${parseFloat(cedula).toFixed(2)}`)
+  })
 
-}
+  }
+
+
+
+
+
+
+teste()
